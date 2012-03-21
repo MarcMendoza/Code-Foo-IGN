9121f180f413ffd20edc988281d55f363d4a44bd
Marc Mendoza <synth477@gmail.com>
2012-03-21 17:34:39 -0600

    Title Screen

--- Bonus_Connect_4/con4.js
+++ Bonus_Connect_4/con4.js
@@ -2,41 +2,55 @@
 //Code Foo IGN
 //Bonus: Connect 4
 
-var gloop;
+//retrieve Canvas from html
+var canvas = document.getElementById("Canvas");
+var ctx = canvas.getContext("2d");
+
+var gloop,
+	width = canvas.width,
+	height = canvas.height,
 //int to keep track of current game state
 // 1: Title Menu
 // 2: In-Game
 // 3: Pause Menu
 // 4: Finished Screen
-var curGameState;
+	curGameState = 1;
 
-//retrieve Canvas from html
-var c = document.getElementById("Canvas");
-var ctx = c.getContext("2d");
-
-//clear function
+//clears the screen with white
 var clear = function(){
-	ctx.fillStyle="#FFFFFF";
-	
-	ctx.beginPath();
-	ctx.rect(0, 0, c.width, c.height);
-	ctx.closePath();
-	
-	ctx.fill();
-}
+	ctx.clearRect(0, 0, width, height);
+};
 
+//
 var TitleScreen = function(){
-	ctx.font="10pt Arial";
-	ctx.fillText("Connect 4: Pixelated Edition", c.width/2
-}
+	var diffSelec = [ctx.measureText("Easy"),
+					ctx.measureText("Medium"),
+					ctx.measureText("Hard")];
+
+	ctx.fillStyle = "#000000";
+	ctx.textAlign = "center";
+	
+	ctx.font="40px Comic Sans MS";
+	ctx.fillText("Connect 4", width/2, height/3-30);
+	ctx.fillText("Pixelated Edition", width/2, height/3 +10);
+	
+	ctx.font="20px Comic Sans MS";
+	ctx.fillText("Select Difficulty:", width/4+30, height - height/4 - 15);
+	
+	ctx.textAlign = "left";
+	ctx.fillStyle = "#999";
+	ctx.fillText("Easy", width - width/4 - 35, height - height/4 - 45);
+	ctx.fillText("Medium", width - width/4 - 35, height - height/4 - 15);
+	ctx.fillText("Hard", width - width/4 - 35, height - height/4 + 15);
+};
 
 var GameLoop = function(){
 	clear();
-	
 	//handles which set of logic is called
 	switch(curGameState)
 	{
 		case 1: //Title Menu
+			TitleScreen();
 			break;
 		case 2: //In-Game
 			break;
@@ -47,4 +61,6 @@ var GameLoop = function(){
 	}
 	
 	gLoop = setTimeout(GameLoop, 1000/30);
-}
+};
+
+GameLoop();
