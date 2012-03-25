e0eea5f11d90ba88ca1789bde6e0315f85a4c107
Marc Mendoza <synth477@gmail.com>
2012-03-25 14:50:34 -0600

    mouseover added

--- Bonus_Connect_4/con4.js
+++ Bonus_Connect_4/con4.js
@@ -9,6 +9,8 @@ var ctx = canvas.getContext("2d");
 var gloop,
 	width = canvas.width,
 	height = canvas.height,
+	mouseX, mouseY,
+	optionSelected = 0,
 //int to keep track of current game state
 // 1: Title Menu
 // 2: In-Game
@@ -21,12 +23,23 @@ var clear = function(){
 	ctx.clearRect(0, 0, width, height);
 };
 
+var OnMouseOver = function onMouseover(e){
+	mouseX = e.clientX - canvas.clientLeft;
+	mouseY = e.clientY - canvas.clientTop;
+};
+
 //
-var TitleScreen = function(){
+var UpdateTitleScreen = function(){
 	var diffSelec = [ctx.measureText("Easy"),
 					ctx.measureText("Medium"),
 					ctx.measureText("Hard")];
 
+	if(mouseX < (width - width/4 - 35 + diffSelec[2]))
+	{
+	}
+};
+
+var DrawTitleScreen = function(){
 	ctx.fillStyle = "#000000";
 	ctx.textAlign = "center";
 	
@@ -44,23 +57,59 @@ var TitleScreen = function(){
 	ctx.fillText("Hard", width - width/4 - 35, height - height/4 + 15);
 };
 
+var UpdateInGame = function(){
+};
+
+var DrawInGame = function(){
+};
+
+var UpdatePauseMenu = function(){
+};
+
+var DrawPauseMenu = function(){
+};
+
+var UpdateFinishScreen = function(){
+};
+
+var DrawFinishScreen = function(){
+};
+
 var GameLoop = function(){
-	clear();
+	clear(); //clear screen of previous draws
+	
 	//handles which set of logic is called
+	//Logic and Draw is seperated in order to make functions
+	//smaller to be better defined
 	switch(curGameState)
 	{
 		case 1: //Title Menu
-			TitleScreen();
+			UpdateTitleScreen();
+			DrawTitleScreen();
 			break;
 		case 2: //In-Game
+			UpdateInGame();
+			DrawInGame();
 			break;
 		case 3: //Pause Menu
+			UpdatePauseMenu();
+			DrawPauseMenu();
 			break;
 		case 4: //Finished Screen
+			UpdateFinishScreen();
+			DrawFinishScreen();
 			break;
 	}
 	
 	gLoop = setTimeout(GameLoop, 1000/30);
 };
 
-GameLoop();
+//sets up event listeners for mouseover and click
+//then starts the GameLoop function
+var init = function(){
+	addEventListener("mouseover", OnMouseOver, false);
+	//need event listener for mouse click
+	GameLoop();
+};
+
+init();
